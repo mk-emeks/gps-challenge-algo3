bb3055cfc7d63a12a0d4def5edcd183ca973146f
turco807@gmail.com <turco807@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-01 19:27:24 +0000

    A modo de "juego" aplique herencia

--- trunk/src/fiuba/algo3/ejemplo1/Eze.java
+++ trunk/src/fiuba/algo3/ejemplo1/Eze.java
@@ -1,7 +1,7 @@
 package fiuba.algo3.ejemplo1;
 
 
-public class Eze {
+public class Eze extends Persona {
 
     public String devolverMiNombre(){
 
---
+++ trunk/src/fiuba/algo3/ejemplo1/Persona.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.ejemplo1;
+
+
+public abstract class Persona {
+
+    public abstract String devolverMiNombre();
+}
