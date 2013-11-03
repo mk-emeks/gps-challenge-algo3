bf505f8b57e7122bb471acf2d2e9ee401e163bb9
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-03 18:08:38 +0000

    Arrancamos. Creado el paquete modelo. Creada la clase Estado y sus descendientes con un metodo basico.

---
+++ trunk/src/fiuba/algo3/modelo/EstadoCalle.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.modelo;
+
+
+public class EstadoCalle extends Estado {
+
+    public boolean sosAlojable() {
+        return true;
+    }
+}
---
+++ trunk/src/fiuba/algo3/modelo/EstadoCuadra.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.modelo;
+
+
+public class EstadoCuadra extends Estado {
+
+    public boolean sosAlojable() {
+        return false;
+    }
+}
