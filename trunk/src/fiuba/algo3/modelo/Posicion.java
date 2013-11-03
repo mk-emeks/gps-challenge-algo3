c18d23af7f28c7a33a60a361a165742730e9a902
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-03 19:14:25 +0000

    Creada la clase Posicion con algunos metodos basicos y sus respectivos tests.

---
+++ trunk/src/fiuba/algo3/modelo/Posicion.java
@@ -0,0 +1,57 @@
+package fiuba.algo3.modelo;
+
+
+public class Posicion {
+
+    private double posicionX;
+    private double posicionY;
+
+    public Posicion() {
+        this.posicionX = 0.0;
+        this.posicionY = 0.0;
+    }
+
+    public Posicion(double unaPosicionX, double unaPosicionY) {
+        this.posicionX = unaPosicionX;
+        this.posicionY = unaPosicionY;
+    }
+
+    public double getPosicionX() {
+        return this.posicionX;
+    }
+
+    public double getPosicionY() {
+        return this.posicionY;
+    }
+
+    public Posicion sumar(Posicion unaPosicion){
+
+        double posicionFinalX = (this.getPosicionX() + unaPosicion.getPosicionX());
+        double posicionFinalY = (this.getPosicionY() + unaPosicion.getPosicionY());
+
+        Posicion posicionFinal = new Posicion(posicionFinalX, posicionFinalY);
+        return posicionFinal;
+    }
+
+    public Posicion restar(Posicion unaPosicion){
+
+        double posicionFinalX = (this.getPosicionX() - unaPosicion.getPosicionX());
+        double posicionFinalY = (this.getPosicionY() - unaPosicion.getPosicionY());
+
+        Posicion posicionFinal = new Posicion(posicionFinalX, posicionFinalY);
+        return posicionFinal;
+    }
+
+
+    //Compara solo coordenadas.
+    public boolean comparar(Posicion unaPosicion) {
+        boolean iguales = false;
+
+        if ( (this.getPosicionX() == unaPosicion.getPosicionX()) && (this.getPosicionX() == unaPosicion.getPosicionX())) {
+            iguales = true;
+        }
+
+        return iguales;
+
+    }
+}
---
+++ trunk/test/fiuba/algo3/modelo/PosicionTest.java
@@ -0,0 +1,45 @@
+package fiuba.algo3.modelo;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class PosicionTest {
+
+    @Test
+    public void testDeberiaCrearseUnaPosicionConDeterminadosParametros() {
+        Posicion unaPosicion = new Posicion(2.0,3.5);
+
+        Assert.assertEquals(2.0, unaPosicion.getPosicionX()); //No esta bueno esto de los dos asserts pero es lo mas sencillo.
+        Assert.assertEquals(3.5, unaPosicion.getPosicionY());
+    }
+
+    @Test
+    public void testDeberiaSumarseUnaPosicionConOtra() {
+        Posicion posicionUno = new Posicion(2.0,1.0);
+        Posicion posicionDos = new Posicion(3.0,-6.0);
+
+        Posicion posicionFinal = posicionUno.sumar(posicionDos);
+
+        Assert.assertEquals(5.0, posicionFinal.getPosicionX());
+        Assert.assertEquals(-5.0, posicionFinal.getPosicionY());
+    }
+
+    @Test
+    public void testDeberiaRestarseUnaPosicionConOtra() {
+        Posicion posicionUno = new Posicion(2.0,1.0);
+        Posicion posicionDos = new Posicion(3.0,-6.0);
+
+        Posicion posicionFinal = posicionUno.restar(posicionDos);
+
+        Assert.assertEquals(-1.0, posicionFinal.getPosicionX());
+        Assert.assertEquals(7.0, posicionFinal.getPosicionY());
+    }
+
+    @Test
+    public void testDeberiaPoderCompararDosPosiciones() {
+        Posicion posicionUno = new Posicion (2.0,1.0);
+        Posicion posicionDos = new Posicion (3.0,1.0);
+
+        Assert.assertFalse(posicionUno.comparar(posicionDos));
+    }
+}
