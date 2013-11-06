c4cf6980d13d253a880765139eb9c47be9b981f1
ezequielrozen@gmail.com <ezequielrozen@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-06 05:30:08 +0000

    Algunas plantillas de interfaces y clases que seran implementadas cuando se termine de implementar el metodo poner de la clase Tablero.

---
+++ src/fiuba/algo3/modelo/Posicionable.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.modelo;
+
+
+public interface Posicionable {
+    /*esta interface la implementa
+      los elementos que metamos en el tablero
+      vehiculos, sorpresas y obstaculos*/
+    //void ponerEn(Tablero tablero);
+}
