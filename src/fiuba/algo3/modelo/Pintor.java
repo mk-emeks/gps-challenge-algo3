a8be80bd6a5808c3715d746928ea1ff4cc930e32
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-06 16:13:43 +0000

    Se saco un responsabilidades a la clase EditorTablero, se creo la clase Pintor.

---
+++ src/fiuba/algo3/modelo/Pintor.java
@@ -0,0 +1,64 @@
+package fiuba.algo3.modelo;
+
+
+public class Pintor {
+
+    private EstadoCuadra pinturaCuadra = new EstadoCuadra();
+    private EstadoCalle pinturaCalle = new EstadoCalle();
+
+    public void pintar(Casilla unaCasilla, EstadoCasilla unEstadoCasilla) {
+
+        unaCasilla.setEstado(unEstadoCasilla);
+    }
+
+    public void pintarUnaCalleEn(Casilla unaCasilla){
+
+        unaCasilla.setEstado(pinturaCalle);
+    }
+
+    public void pintarUnaCuadraEn(Casilla unaCasilla) {
+
+        unaCasilla.setEstado(pinturaCuadra);
+    }
+
+    private void pintarCallesImpares(Tablero unTablero) {
+
+        for(int i = 0; i < unTablero.cantidadDeFilas(); i=i+2) {
+            for(int j = 1; j < unTablero.cantidadDeColumnas(); j=j+2) {
+
+                this.pintarUnaCalleEn(unTablero.getCasilla(i,j));
+            }
+        }
+    }
+
+    private void pintarCallesPares(Tablero unTablero) {
+
+        for(int i = 1; i < unTablero.cantidadDeFilas(); i=i+2) {
+            for(int j = 0; j < unTablero.cantidadDeColumnas(); j++) {
+
+                this.pintarUnaCalleEn(unTablero.getCasilla(i,j));
+            }
+        }
+    }
+
+    private void pintarCuadrasSimplesA(Tablero unTablero){
+
+        for(int i = 0; i < unTablero.cantidadDeFilas(); i=i+2) {
+            for(int j = 0; j < unTablero.cantidadDeColumnas(); j=j+2) {
+
+                this.pintarUnaCuadraEn(unTablero.getCasilla(i,j));
+            }
+        }
+    }
+
+    private void pintarCallesSimplesA(Tablero unTablero) {
+
+        this.pintarCallesImpares(unTablero);
+        this.pintarCallesPares(unTablero);
+    }
+
+    public void pintarTableroSimple(Tablero unTablero) {
+        this.pintarCuadrasSimplesA(unTablero);
+        this.pintarCallesSimplesA(unTablero);
+    }
+}
