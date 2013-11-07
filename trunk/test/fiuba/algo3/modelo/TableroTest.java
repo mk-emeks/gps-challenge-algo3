873a0e2827bd9eef5e762925afbe42a7ef5412dd
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-07 16:12:33 +0000

    Reparacion del verificador de coordenadas en la clase Tablero, por eso pinchaba la clase Pintor. Al parecer todo OK ahora.

--- trunk/test/fiuba/algo3/modelo/TableroTest.java
+++ trunk/test/fiuba/algo3/modelo/TableroTest.java
@@ -2,6 +2,7 @@ package fiuba.algo3.modelo;
 
 import junit.framework.Assert;
 import org.junit.Test;
+
 public class TableroTest {
 
     @Test
@@ -18,7 +19,12 @@ public class TableroTest {
     public void deberiaCrearCasillasEnLaUltimaPosicionCorrespondienteSegunLosParametros() {
         Tablero tablero = new Tablero(12,11);
         Assert.assertNotNull(tablero.getCasilla(11,10));
+    }
 
+    @Test
+    public void testDeberiaPoderDecirmeSiUnaCoordenadaEstaEnElTablero() {
+        Tablero tablero = new Tablero (4,20);
+        Assert.assertTrue(tablero.coordenadaDentroDelTablero(3,19));
     }
 
 }
