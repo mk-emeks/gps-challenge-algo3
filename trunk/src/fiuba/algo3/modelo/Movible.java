e5fd0b24e581c9dfd6d6c5747e205bdd036c8dd3
ezequielrozen@gmail.com <ezequielrozen@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-08 20:43:30 +0000

    Agregado de Vehiculos, interfaces Movible, Posicionable e Interceptable

---
+++ trunk/src/fiuba/algo3/modelo/Movible.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.modelo;
+
+
+public interface Movible extends Posicionable {
+
+    void moverEn(Mapa unMapa);
+}
---
+++ trunk/src/fiuba/algo3/modelo/Vehiculo.java
@@ -0,0 +1,36 @@
+package fiuba.algo3.modelo;
+
+public abstract class Vehiculo implements Movible, Interceptable {
+
+    private Posicion posicion;
+    private Direccion direccion;
+
+    public Vehiculo(Direccion unaDireccion, Posicion unaPosicion) {
+        this.posicion = unaPosicion;
+        this.direccion = unaDireccion;
+    }
+
+    public void setDireccion(Direccion nuevaDireccion) {
+        this.direccion = nuevaDireccion;
+    }
+
+    public Direccion getDireccion() {
+        return this.direccion;
+    }
+
+    public Posicion getPosicion() {
+        return this.posicion;
+    }
+
+    public void setPosicion(Posicion nuevaPosicion) {
+        this.posicion = nuevaPosicion;
+    }
+
+    public void moverEn(Mapa unMapa) {
+
+    }
+
+
+
+
+}
