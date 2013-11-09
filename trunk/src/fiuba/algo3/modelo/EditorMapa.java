3845678a9b99f91cd11e130376d50e88d60b55c2
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-09 19:40:14 +0000

    Modificaciones en las clases Auto, CuatroPorCuatro y Moto, los parametros del constructor al llamar al super estaba al reves. Modificacion clase EditorMapa al momento de las iteraciones. Creada clase EditorMapaTest que verifica varios casos posibles.

--- trunk/src/fiuba/algo3/modelo/EditorMapa.java
+++ trunk/src/fiuba/algo3/modelo/EditorMapa.java
@@ -1,14 +1,5 @@
 package fiuba.algo3.modelo;
 
-/** el "pintor" disfrazado, si obvio papa, volvio en forma de editor,
- * EDU: Un gran tema, cuando hiciste los metodos logre ver que al final cuando pintabas la casilla asignabas (i,j)
- * Si i es fila y j es columna, asignabas al reves de como esta implementada nuestra posicion, pero al mismo tiempo
- * funcionaba, asique nose..De tu codigo lo copie igual pero inverti la ultima asignacion porque supongo que el
- * algoritmo lo pensaste siendo conciente de que i es fila y j columna, seguramente anduvo antes
- * porque teniamos un mapa pintado "al reves" de lo que pensabamos. -> trata de fijarte que onda
- *
- * pd. te dejo que le hagas los test a tu hijo querido jajajaja
- */
 public class EditorMapa {
 
     /** temporal: uso para probar el mapa plantiado en la consigna inicial **/
@@ -22,8 +13,8 @@ public class EditorMapa {
 
     private void agregarCallesImpares(Mapa map, int columnas, int filas) throws LaPosicionYaExisteEnElMapaException {
 
-        for(int i = 0; i < filas; i=i+2) {
-            for(int j = 1; j < columnas; j=j+2) {
+        for(int i = 1; i <= filas; i=i+2) {
+            for(int j = 2; j <= columnas; j=j+2) {
 
                 map.agregar(new Posicion(j,i));  /**!**/
             }
@@ -32,8 +23,8 @@ public class EditorMapa {
 
     private void agregarCallesPares(Mapa map, int columnas, int filas) throws LaPosicionYaExisteEnElMapaException {
 
-        for(int i = 1; i < filas; i=i+2) {
-            for(int j = 0; j < columnas; j++) {
+        for(int i = 2; i <= filas; i=i+2) {
+            for(int j = 1; j <= columnas; j++) {
 
                 map.agregar(new Posicion(j,i));  /**!**/
             }
