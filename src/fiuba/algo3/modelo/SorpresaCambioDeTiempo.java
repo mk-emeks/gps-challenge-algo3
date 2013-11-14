b999c6ea983b43c41cbb590e4a8b465eb629bc9c
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-14 04:46:55 +0000

    Creo clase SorpresaCambioDeTiempo para Mirko

---
+++ src/fiuba/algo3/modelo/SorpresaCambioDeTiempo.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.modelo;
+
+
+public class SorpresaCambioDeTiempo extends Sorpresa {
+
+    int porcentajeTiempo;
+
+    //public SorpresaCambioDeTiempo (int porcentaje, Posicion unaPosicion) {
+
+    //    this.porcentajeTiempo = porcentaje;
+    //    //super(unaPosicion);
+    //}
+
+
+}
