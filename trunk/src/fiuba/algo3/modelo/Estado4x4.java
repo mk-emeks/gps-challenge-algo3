38833f13534d15efa0829f6812363b9f772db589
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-14 02:46:25 +0000

    Estados here we go again.

---
+++ trunk/src/fiuba/algo3/modelo/Estado4x4.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.modelo;
+
+
+public class Estado4x4 extends Estado {
+}
---
+++ trunk/src/fiuba/algo3/modelo/EstadoAuto.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.modelo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrador
+ * Date: 13/11/13
+ * Time: 23:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class EstadoAuto extends Estado {
+}
