2b3c650b58b3c51cb01864bb24a1050f0d00f258
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-15 13:47:35 +0000

    PiqueteTest actualizada y modificaciones no importantes en PozoTest

--- test/fiuba/algo3/modelo/PiqueteTest.java
+++ test/fiuba/algo3/modelo/PiqueteTest.java
@@ -3,9 +3,19 @@ package fiuba.algo3.modelo;
 
 import junit.framework.Assert;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class PiqueteTest {
+
+    Mapa unMapa;
+
+    @Before
+    public void setUp() {
+        unMapa = Mapa.getMapa();
+    }
 
     @After
     public void limpiezaMapa() {
@@ -13,11 +23,124 @@ public class PiqueteTest {
     }
 
     @Test
-    public void testDeberiaCrearUnPozoConLaUnaPosicionYSuPenalizacion() {
-        Posicion unaPosicion = new Posicion(2,2);
-        Piquete unPiquete = new Piquete(unaPosicion);
+    public void testDeberiaCrearUnPiqueteConUnaDeterminadaPosicion() {
+        Posicion unaPosicion = new Posicion(5,1);
+        Piquete unPiquete = new Piquete(new Posicion(5,1));
 
-        Assert.assertEquals(unPiquete.getPosicion().getPosicionX(), 2);
-        Assert.assertEquals(unPiquete.getPosicion().getPosicionY(),2);
+        Assert.assertTrue(unPiquete.getPosicion().equals(unaPosicion));
+        Assert.assertEquals(2, unPiquete.cantidadDeTurnosPenalizado());
+    }
+
+    @Test
+    public void testDeberiaCrearUnPiqueteConUnaDeterminadaPosicionYUbicarloEnUnMapa() throws Exception{
+        EditorMapa unEditor = new EditorMapa();
+        unEditor.editarMapaMinimoJugable();
+
+        Piquete unPiquete = new Piquete(new Posicion(2,2));
+        unPiquete.posicionarEnElMapa();
+
+        Assert.assertTrue(unMapa.estaUbicado(unPiquete));
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Test
+    public void testDeberiaLanzarUnaExceptionAlQuererUbicarUnPiqueteEnUnaPosicionFueraDelMapa() throws Exception {
+        EditorMapa unEditor = new EditorMapa();
+        unEditor.editarMapaMinimoJugable();
+
+        Piquete unPiquete = new Piquete(new Posicion(4,4));
+
+        thrown.expect(NoSePuedePonerEnElMapaElPosicionableException.class);
+        unPiquete.posicionarEnElMapa();
+    }
+
+    @Rule
+    public ExpectedException thrown1 = ExpectedException.none();
+    @Test
+    public void testDeberiaLanzarUnaExceptionAlQuererUbicarUnPiqueteEnUnaPosicionCuadra() throws Exception {
+        EditorMapa unEditor = new EditorMapa();
+        unEditor.editarMapaMinimoJugable();
+
+        Piquete unPiquete = new Piquete(new Posicion(1,1));
+
+        thrown1.expect(NoSePuedePonerEnElMapaElPosicionableException.class);
+        unPiquete.posicionarEnElMapa();
+    }
+
+    @Test
+    public void testDeberiaInvertirDireccionAUnVehiculoAuto() throws Exception{
+        EditorMapa unEditor = new EditorMapa();
+        unEditor.editarMapaMinimoJugable();
+
+        Posicion posicionInicial = new Posicion(1,2);
+        Direccion direccion = new DireccionDerecha();
+        EstadoAuto unEstadoAuto = new EstadoAuto();
+
+        Vehiculo unAuto = new Vehiculo(posicionInicial, direccion, unEstadoAuto);
+        Piloto unPiloto = new Piloto(unAuto);
+        unAuto.posicionarEnElMapa();
+        unPiloto.arrancarVehiculo();
+
+        Piquete unPiquete = new Piquete(new Posicion(2,2));
+        unPiquete.posicionarEnElMapa();
+
+        Assert.assertTrue(unPiloto.getVehiculo().getDireccion().equals(new DireccionDerecha()));
+        unPiquete.aplicarA(unPiloto, (EstadoAuto)unAuto.getEstado()); //Le aseguro al compilador que es un EstadoAuto
+
+        Assert.assertTrue(unPiloto.getVehiculo().getDireccion().equals(new DireccionIzquierda()));
+    }
+
+    @Test
+    public void testDeberiaInvertirDireccionAUnVehiculo4x4() throws Exception{
+        EditorMapa unEditor = new EditorMapa();
+        unEditor.editarMapaMinimoJugable();
+
+        Posicion posicionInicial = new Posicion(1,2);
+        Direccion direccion = new DireccionAbajo();
+        Estado4x4 unEstado4x4 = new Estado4x4();
+
+        Vehiculo unaCamioneta = new Vehiculo(posicionInicial, direccion, unEstado4x4);
+        Piloto unPiloto = new Piloto(unaCamioneta);
+        unaCamioneta.posicionarEnElMapa();
+        unPiloto.arrancarVehiculo();
+
+        Piquete unPiquete = new Piquete(new Posicion(2,2));
+        unPiquete.posicionarEnElMapa();
+
+        Assert.assertTrue(unPiloto.getVehiculo().getDireccion().equals(new DireccionAbajo()));
+        unPiquete.aplicarA(unPiloto, (Estado4x4)unaCamioneta.getEstado()); //Le aseguro al compilador que es un Estado4x4
+
+        Assert.assertTrue(unPiloto.getVehiculo().getDireccion().equals(new DireccionArriba()));
+    }
+
+    @Test
+    public void testDeberiaPoderPenalizarAUnVehiculoMotoYLuegoDejarloAvanzar() throws Exception{
+        EditorMapa unEditor = new EditorMapa();
+        unEditor.editarMapaMinimoJugable();
+
+        Posicion posicionInicial = new Posicion(2,2);
+        Direccion direccion = new DireccionIzquierda();
+        EstadoMoto unEstadoMoto = new EstadoMoto();
+
+        Vehiculo unaMoto = new Vehiculo(posicionInicial, direccion, unEstadoMoto);
+        Piloto unMotoquero = new Piloto(unaMoto);
+        unaMoto.posicionarEnElMapa();
+        unMotoquero.arrancarVehiculo();
+
+        Pozo unPozo = new Pozo(new Posicion(2,2));
+        unPozo.posicionarEnElMapa();
+
+        int numeroPenalizacion = unPozo.cantidadDeTurnosPenalizado();
+
+        for(int i =1; i<=numeroPenalizacion;i++) {
+            unPozo.aplicarA(unMotoquero, (EstadoMoto)unaMoto.getEstado()); //Le aseguro al compilador que es un EstadoMoto
+            unMotoquero.conducir();
+            Assert.assertTrue(unMotoquero.getVehiculo().getPosicion().equals(posicionInicial));
+        }
+        /**Ahora no deberia afectarme el piquete **/
+        unPozo.aplicarA(unMotoquero, (EstadoMoto)unaMoto.getEstado());
+        unMotoquero.conducir();
+        Assert.assertTrue(unMotoquero.getVehiculo().getPosicion().equals(new Posicion(1,2)));
     }
 }
