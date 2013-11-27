869f65a196d8ac5928e3d45253eed181fcbb7c37
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-27 21:53:17 +0000

    VistaInicio, cambio en Partida, imagen Inicio

---
+++ src/fiuba/algo3/vista/VistaInicio.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.vista;
+
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class VistaInicio extends Imagen {
+
+    public VistaInicio() {
+
+        this.setNombreArchivoImagen("imagenes/largada.png");
+
+    }
+}
