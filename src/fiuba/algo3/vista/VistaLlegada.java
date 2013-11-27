ea396b9b7fde4aaa5391c6710106ecde367378d5
turco807@gmail.com <turco807@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-27 21:31:16 +0000

    vistas nuevas

---
+++ src/fiuba/algo3/vista/VistaLlegada.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.vista;
+
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class VistaLlegada extends Imagen {
+
+    public VistaLlegada () {
+
+        this.setNombreArchivoImagen("imagenes/FIUBA.png");
+
+    }
+}
