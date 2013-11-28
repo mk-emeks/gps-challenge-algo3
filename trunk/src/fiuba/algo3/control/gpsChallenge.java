444ec34d44fe5ea37c177ddb398e42ac5c9b4d30
turco807@gmail.com <turco807@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-11-28 17:10:58 +0000

    gps challenge el nuevo "juego"

---
+++ trunk/src/fiuba/algo3/control/gpsChallenge.java
@@ -0,0 +1,49 @@
+package fiuba.algo3.control;
+
+
+import ar.uba.fi.algo3.titiritero.ControladorJuego;
+import ar.uba.fi.algo3.titiritero.vista.MouseClickController;
+import ar.uba.fi.algo3.titiritero.vista.Panel;
+import ar.uba.fi.algo3.titiritero.vista.Ventana;
+import fiuba.algo3.modelo.Nivel;
+import fiuba.algo3.modelo.Piloto;
+import fiuba.algo3.modelo.Mapa;
+
+import java.awt.*;
+
+
+public class gpsChallenge {
+
+
+
+    public static void main(String[] argv) {
+
+        /** creamos el control de juego **/
+        ControladorJuego controlDeJuego = new ControladorJuego(false);
+        controlDeJuego.setIntervaloSimulacion(90);
+
+        /** Configuracion Ventana **/
+        Ventana ventana = new Ventana(controlDeJuego);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        ventana.setLayout(null);
+        ventana.setBounds(0, 0, screenSize.width, screenSize.height);
+        ventana.setVisible(true);
+
+        /** agregamos observador de mouse **/
+        ventana.addMouseListener(new MouseClickController(controlDeJuego));
+
+        /** vamos a jugar **/
+        Mapa.limpiar();
+        Nivel nivel = new Nivel();
+        Piloto piloto = new Piloto("pilotin");
+        Partida partida = new Partida(ventana,controlDeJuego,nivel,piloto);
+
+        partida.iniciar();
+
+
+    }
+
+
+
+
+}
