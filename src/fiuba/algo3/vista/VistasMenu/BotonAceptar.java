810fdcd96753bc5dd8b0fb756a3cc650d44f00be
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-12-07 23:32:04 +0000

    Boton Aceptar

---
+++ src/fiuba/algo3/vista/VistasMenu/BotonAceptar.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.vista.VistasMenu;
+
+import javax.swing.*;
+
+
+public class BotonAceptar extends Boton {
+
+    public BotonAceptar(JFrame unMarco, int posicionX, int posicionY, int ancho, int largo) {
+        super(unMarco, posicionX, posicionY, ancho, largo);
+    }
+}
