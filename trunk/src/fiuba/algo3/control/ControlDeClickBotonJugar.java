4f30df7087e6b6badfdd3bfb90cef71a6c883b72
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-12-08 05:06:57 +0000

    Se vienen las Instrucciones

--- trunk/src/fiuba/algo3/control/ControlDeClickBotonJugar.java
+++ trunk/src/fiuba/algo3/control/ControlDeClickBotonJugar.java
@@ -1,16 +1,14 @@
 package fiuba.algo3.control;
 
 
-import fiuba.algo3.modelo.EstadoMoto;
 //import fiuba.algo3.vista.VistasMenu.BotonAnimadoVehiculo;
 import fiuba.algo3.vista.VistasMenu.BotonAnimadoVehiculo;
+import fiuba.algo3.vista.VistasMenu.VistaInstrucciones;
 import fiuba.algo3.vista.VistasMenu.VistaFinalizacion;
-import fiuba.algo3.vista.VistasMenu.VistaMenu;
+        import fiuba.algo3.vista.VistasMenu.VistaMenu;
 
-import javax.swing.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
+        import java.awt.event.MouseEvent;
+        import java.util.ArrayList;
 
 
 public class ControlDeClickBotonJugar extends ControlDeClickBotonConPartida {
@@ -42,6 +40,7 @@ public class ControlDeClickBotonJugar extends ControlDeClickBotonConPartida {
         this.partida.cargarVehiculoParaElPiloto();
         this.partida.iniciar();
         this.partida.pausar();
+        VistaInstrucciones unaVistaInstrucciones = new VistaInstrucciones();
 
         this.actualizar();
         this.botonAnimadoVehiculo.comenzarAnimacion(); // el orden es importante, tiene que ser despues!
