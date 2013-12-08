951609c25b0bc159821246ec61673e2be97a5653
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-12-08 05:45:36 +0000

    Instrucciones completo

--- trunk/src/fiuba/algo3/vista/VistasMenu/VistaInstrucciones.java
+++ trunk/src/fiuba/algo3/vista/VistasMenu/VistaInstrucciones.java
@@ -10,28 +10,18 @@ public class VistaInstrucciones {
 
 
         JFrame unFrame = new JFrame();
-        unFrame.setBounds(700,300,400,300);
+        unFrame.setBounds(50,50,770,700);
+        unFrame.setTitle("Instrucciones");
         unFrame.setLayout(null);
         unFrame.setDefaultCloseOperation(unFrame.DISPOSE_ON_CLOSE);
-        /** SE LEVANTA UNA IMAGEN CON INSTRUCCIONES
-         *
-        JLabel labelMensaje1 = new JLabel();
-        labelMensaje1.setText("Ese usuario ya estaba registrado");
-        labelMensaje1.setForeground(new Color(197,0, 23));
-        labelMensaje1.setFont(new Font(Font.SERIF, Font.BOLD, 20));
-        labelMensaje1.setBounds(10,0,400,30);
-        labelMensaje1.setVisible(true);
 
-        JLabel labelMensaje2 = new JLabel();
-        labelMensaje2.setText("Cerrar para continuar");
-        labelMensaje2.setForeground(new Color(197,0, 23));
-        labelMensaje2.setFont(new Font(Font.SERIF, Font.BOLD, 10));
-        labelMensaje2.setBounds(10,30,400,30);
-        labelMensaje2.setVisible(true);
 
-        unFrame.add(labelMensaje1);
-        unFrame.add(labelMensaje2);**/
+        JLabel instrucciones = new JLabel();
+        instrucciones.setBounds(0,0,770,670);
+        instrucciones.setIcon(new ImageIcon("src/fiuba/algo3/vista/imagenes/instrucciones.png"));
+        instrucciones.setVisible(true);
 
+        unFrame.add(instrucciones);
         unFrame.setVisible(true);
     }
 }
