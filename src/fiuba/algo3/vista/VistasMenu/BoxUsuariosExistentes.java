70d87ffafcb79ad1ea4ab0d9079f8842730f1685
e.calcabrini@gmail.com <e.calcabrini@gmail.com@448b57a7-835a-d296-ab8d-2377d74dadae>
2013-12-09 13:24:58 +0000

    Pelotudeces

--- src/fiuba/algo3/vista/VistasMenu/BoxUsuariosExistentes.java
+++ src/fiuba/algo3/vista/VistasMenu/BoxUsuariosExistentes.java
@@ -18,18 +18,21 @@ public class BoxUsuariosExistentes extends Box {
         ArrayList<Usuario> listaUsuarios = registroUsuarios.getUsuarios();
 
         try {
-        this.itembox1.setText(listaUsuarios.get(0).getNombre());
-        this.itembox1.setForeground(Color.RED);
+            this.itembox1.setText(listaUsuarios.get(0).getNombre());
+            this.itembox1.setForeground(new Color(197, 104, 13));
+            this.itembox1.setFont(new Font(Font.SERIF, Font.BOLD, 20));
         } catch (Exception e1) { System.out.println("error al cargar el nombre del 1er usuario"); }
 
         try {
-        this.itembox2.setText(listaUsuarios.get(1).getNombre());
-        this.itembox2.setForeground(Color.RED);
+            this.itembox2.setText(listaUsuarios.get(1).getNombre());
+            this.itembox2.setForeground(new Color(197, 104, 13));
+            this.itembox2.setFont(new Font(Font.SERIF, Font.BOLD, 20));
         } catch (Exception e1) { System.out.println("error al cargar el nombre del 2er usuario"); }
 
         try{
-        this.itembox3.setText(listaUsuarios.get(2).getNombre());
-        this.itembox3.setForeground(Color.RED);
+            this.itembox3.setText(listaUsuarios.get(2).getNombre());
+            this.itembox3.setForeground(new Color(197, 104, 13));
+            this.itembox3.setFont(new Font(Font.SERIF, Font.BOLD, 20));
         } catch (Exception e1) { System.out.println("error al cargar el nombre del 3er usuario"); }
 
 
